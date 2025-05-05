1dbb7f856e708d03473445af5834ee199e19edfd
bk2111987 <b.khatib87@gmail.com>
2025-05-05 08:48:36 -0400

    Add backend/models/user.js 1746449316344

---
+++ backend/models/user.js
@@ -0,0 +1,75 @@
+const { DataTypes } = require('sequelize');
+const bcrypt = require('bcryptjs');
+
+module.exports = (sequelize) => {
+  const User = sequelize.define('User', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
+    role: {
+      type: DataTypes.ENUM('contractor', 'vendor', 'subcontractor', 'customer', 'admin'),
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+      validate: {
+        isEmail: true,
+      },
+    },
+    passwordHash: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    phone: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    language: {
+      type: DataTypes.ENUM('en', 'fr'),
+      defaultValue: 'en',
+    },
+    twoFactorEnabled: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
+    twoFactorSecret: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    isActive: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: true,
+    },
+  }, {
+    tableName: 'users',
+    timestamps: true,
+    hooks: {
+      beforeCreate: async (user) => {
+        if (user.passwordHash) {
+          const salt = await bcrypt.genSalt(10);
+          user.passwordHash = await bcrypt.hash(user.passwordHash, salt);
+        }
+      },
+      beforeUpdate: async (user) => {
+        if (user.changed('passwordHash')) {
+          const salt = await bcrypt.genSalt(10);
+          user.passwordHash = await bcrypt.hash(user.passwordHash, salt);
+        }
+      },
+    },
+  });
+
+  User.prototype.validatePassword = async function(password) {
+    return bcrypt.compare(password, this.passwordHash);
+  };
+
+  return User;
+};
